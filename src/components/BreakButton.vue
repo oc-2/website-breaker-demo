b77ba3a301f233ead49e3baec7e99bac9b885063
Elias Fakoorian <185859974+oc-2@users.noreply.github.com>
2025-10-10 23:35:04 +0000

    Initial Commit

---
+++ src/components/BreakButton.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="break-button" @click="destroy" >
+     Break Me
+   </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import breakWebPage from "@oc-2/website-breaker";
+import "../assets/BreakButton.css";
+
+export default defineComponent({
+  name: "page-destroyer",
+  props: {
+    msg: String,
+  },
+
+  methods: {
+    destroy(){
+      breakWebPage();
+    }
+  }
+});
+</script>
